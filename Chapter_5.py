935b82d80443e0d26ef9d9b807970c6a4f586fc3
lulu <kuroineko210@hotmail.com>
2018-02-27 00:17:08 -0800

    Camel game start

---
+++ Chapter_5.py
@@ -0,0 +1,4 @@
+print("""Welcom to Camel!
+You have stolen a camel to make your way across the great Mobi desert.
+The natives want their camel back and are chasing you down! Survive your desert trek and out run the natives.
+""")
