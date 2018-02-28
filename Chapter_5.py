8e8ba97ff68811f9cc83f052316be81bb2b90380
Unknown <lulu.qiaoxi.li@gmail.com>
2018-02-27 16:55:58 -0800

    Camel Game
    Up to step 13

--- Chapter_5.py
+++ Chapter_5.py
@@ -1,4 +1,45 @@
+import random
+
 print("""Welcom to Camel!
 You have stolen a camel to make your way across the great Mobi desert.
 The natives want their camel back and are chasing you down! Survive your desert trek and out run the natives.
 """)
+
+#new boolean variable
+done = False
+
+miles_traveled = 0
+thirst = 0
+camel_tiredness = 0
+canteen_drinks = 10
+
+#natives are 20 miles back
+native_distance = -20
+
+while not done:
+    print("""
+    A. Drink from your canteen
+    B. Ahead moderate speed
+    C. Ahead full speed
+    D. Stop for the night
+    E. Status check
+    Q. Quit""")
+
+    user_choice = input("Your choice? ")
+    
+    if user_choice.upper() == "Q":
+        done = True
+    elif user_choice.upper() == "E":
+        print("""
+        Miles traveled: %d
+        Drinks in canteen: %d
+        The natvies are %d miles behind you""" % (miles_traveled, canteen_drinks, -native_distance))
+    elif user_choice.upper() == "D":
+        camel_tiredness = 0
+        print("Camel is happy")
+        native_distance += random.randrange(7, 15)
+    elif user_choice.upper() == "C":
+        miles_traveled = random.randrange(10,21)
+        
+    
+    
